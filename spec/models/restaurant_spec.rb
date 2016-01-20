cbb1f30b9cabb49db75f4a155681de15a50cc892
Richard Moss <richard@englishtutorslive.com>
2016-01-20 10:20:39 +0000

    version one complete, validates reviews

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+
 
 describe Restaurant, type: :model do
   it { is_expected.to have_many :reviews}
+end
+
+describe Restaurant, :type => :model do
+  it 'is not valid with a name of less than three characters' do
+    restaurant = Restaurant.new(name: "kf")
+    expect(restaurant).to have(1).error_on(:name)
+    expect(restaurant).not_to be_valid
+  end
 end
