d05403661e39e54a0994ba6c1d2d4d53cab4a96c
Richard Moss <richard@englishtutorslive.com>
2016-01-22 17:09:47 +0000

    adds endorsments model and controller with tests

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
 
-
   resources :restaurants do
-    resources :reviews
+    resources :reviews do
+      resources :endorsements, shallow: true
+    end
+
+
     devise_scope :user do
       delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
     end
