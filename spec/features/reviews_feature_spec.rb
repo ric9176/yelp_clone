a38f0270411a8ced7c2b7e775d2e27684a4091f8
Richard Moss <richard@englishtutorslive.com>
2016-01-22 11:49:19 +0000

    adds average rating calculation and tests

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -1,16 +1,36 @@
 require 'rails_helper'
 
 feature 'reviewing' do
-  before {Restaurant.create name: 'KFC'}
+  before {Restaurant.create name: 'Mcdonalds'}
+
 
   scenario 'allows users to leave a review using a form' do
     visit '/restaurants'
-    click_link'Review KFC'
+    sign_up
+    click_link'Review Mcdonalds'
     fill_in "Thoughts", with: "so so"
     select '3', from: 'Rating'
     click_button 'Leave Review'
 
     expect(current_path).to eq '/restaurants'
     expect(page).to have_content('so so')
+  end
+
+  scenario 'displays an average rating for all reviews' do
+    visit '/restaurants'
+    sign_up
+    leave_review('So so', '3')
+    click_link 'Sign out'
+    second_sign_up
+    leave_review('Great', '5')
+    expect(page).to have_content('Average rating: 4')
+  end
+
+  def leave_review(thoughts, rating)
+    visit '/restaurants'
+    click_link 'Review Mcdonalds'
+    fill_in 'Thoughts', with: thoughts
+    select rating, from: 'Rating'
+    click_button 'Leave Review'
   end
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -13,3 +13,34 @@ describe Restaurant, :type => :model do
     expect(restaurant).not_to be_valid
   end
 end
+
+describe '#average_rating' do
+  context 'no reviews' do
+    it 'returns "N/A" when there are no reviews' do
+      restaurant = Restaurant.create(name: 'The Ivy')
+      expect(restaurant.average_rating).to eq 'N/A'
+    end
+  end
+
+  context '1 review' do
+    it 'returns that rating' do
+      restaurant = Restaurant.create(name: 'The Ivy')
+      restaurant.reviews.create(rating: 4)
+      expect(restaurant.average_rating).to eq 4
+    end
+  end
+
+
+
+  context 'multiple reviews' do
+    it 'returns the average' do
+      user1 = User.create(email: "test@tits.com", password: 12345678, password_confirmation: 12345678)
+      user2 = User.create(email: "test@test.com", password: 12345678, password_confirmation: 12345678)
+      restaurant = Restaurant.create(name: 'The Ivy')
+      restaurant.reviews.create(rating: 1, user_id: user1.id)
+      restaurant.reviews.create(rating: 5, user_id: user2.id)
+      expect(restaurant.average_rating).to eq 3
+    end
+  end
+
+end
